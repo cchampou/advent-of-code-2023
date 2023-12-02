1c247ba88b7428ec7075430a338f97e3f3c7cf3c
cchampou <clement@champouillon.com>
2023-12-02 22:49:25 +0100

    feat: day two part two

--- day2/bin/main.ml
+++ day2/bin/main.ml
@@ -1,15 +1,16 @@
-let rec process_lines lines index total : int =
-  let game_id = index + 1 in
-  if index = List.length lines then total
-  else
-    try
-      Parser.parse_game_line (List.nth lines index) Constants.get_instructions;
-      Printf.printf "Game %d is possible\n" game_id;
-      process_lines lines (index + 1) (total + game_id)
-    with _ -> process_lines lines (index + 1) total
+let rec part2 lines total : int =
+  match lines with
+  | [] -> total
+  | head :: tail ->
+      let res = Parser.parse_game_line head
+      |> List.fold_left (fun acc x ->
+        acc * fst x
+      ) 1 in
+      part2 tail (total + res)
 
 let () =
   let filename = "input" in
   let lines = Reader.read_file filename in
-  let result = process_lines lines 0 0 in
-  Printf.printf "%d" result
+  part2 lines 0
+  |> Printf.printf "%d"
+
--- day2/bin/parser.ml
+++ day2/bin/parser.ml
@@ -25,15 +25,34 @@ let split_set (set : string) : (int * Types.color) list =
   |> List.map String.trim |> List.map convert_to_pair
 
 let is_valid instructions (x, y) : unit =
-  Printf.printf "Cheking if %d" x;
+  Printf.printf "Cheking if %d is valid for color: " x;
   Logger.log_color y;
   print_endline "";
   let element = List.find (fun s -> snd s = y) instructions in
   if fst element < x then failwith "Not valid" else ()
 
-let check_set instructions set : unit = List.iter (is_valid instructions) set
+let check_set instructions : (int * Types.color) list -> unit =
+  List.iter (is_valid instructions)
 
-let parse_game_line line instructions : unit =
+let max_value_unit (value, color) current_max =
+  if snd current_max == color && value > fst current_max then
+    (value, color)
+  else
+    current_max
+
+let max_value (current_max_table: (int * Types.color) list ) (element: (int * Types.color)) =
+  List.map (max_value_unit element) current_max_table
+
+
+let rec get_max acc sets =
+  match sets with
+  | [] -> acc
+  | head :: tail ->
+      get_max (max_value acc head) tail
+
+let parse_game_line line : (int * Types.color) list =
   get_game_string line |> get_game_str_set |> List.map split_set
   |> List.map Logger.log_set
-  |> List.iter (check_set instructions)
+  |> List.flatten
+  |> get_max [ (0, Types.Red); (0, Types.Blue); (0, Types.Green) ]
+(* |> List.iter (check_set instructions) *)
